715c1f36d259f4b167015ce1fc63dc9659c78417
jagodawojcik <wojcik.jagoda2@gmail.com>
2026-01-11 14:06:45 +0000

    Basic http server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
+}
+
+
+func main() {
+
+	http.HandleFunc("/health", healthHandler)
+
+	fmt.Println("Server running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
